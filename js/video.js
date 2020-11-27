d167f17f2afba6b37b0a7b00ff1e5a89e9b9b165
irmafrat <irmafraticelli412@gmail.com>
2020-11-27 18:45:39 -0500

    play, pause, slow, faster, buttons dibe

--- js/video.js
+++ js/video.js
@@ -6,9 +6,24 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
 });
 
+
+
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	console.log("Pause Video");
+	video.pause();
 });
+
+document.querySelector("#slower").addEventListener("click", function() { 
+	console.log("Slow Down");
+	video.playbackRate = 0.5;
+});
+
+document.querySelector("#faster").addEventListener("click", function() { 
+	console.log("Speed Up");
+	video.playbackRate = 2.0;
+});
+
 

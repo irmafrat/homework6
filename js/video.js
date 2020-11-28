3768ca2065d4d148188eeae97df19f8cc166d744
irmafrat <irmafraticelli412@gmail.com>
2020-11-27 19:25:08 -0500

    volume

--- js/video.js
+++ js/video.js
@@ -2,19 +2,22 @@ var video;
 
 window.addEventListener("load", function() {
 	video = document.querySelector("#myVideo");
+	console.log("Starting");
+	console.log(video.volume);
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	let vol= document.querySelector("#volume").innerHTML = video.volume;
 });
-
 
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
 	video.pause();
 });
+
 
 document.querySelector("#slower").addEventListener("click", function() { 
 	console.log("Slow Down");
